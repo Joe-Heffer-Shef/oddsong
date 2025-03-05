9d005b3fe81a895a42a6ef7c15a6192666723f95
Joe Heffer <60133133+Joe-Heffer-Shef@users.noreply.github.com>
2025-03-05 09:57:31 +0000

    Update identify.R

--- R/identify.R
+++ R/identify.R
@@ -1,4 +1,6 @@
 #' Identify a bird based on the sound of its call.
+#'
+#' @param audio_file The path of the sound file
 identify <- function(audio_file) {
 
     print("Identifying bird vocalisation...")
