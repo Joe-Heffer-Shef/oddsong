175cb60801aa35470a92841bf61bb2abc2c2f5dc
Joe Heffer <60133133+Joe-Heffer-Shef@users.noreply.github.com>
2025-03-05 10:09:55 +0000

    Update song.py

--- oddsong/song.py
+++ oddsong/song.py
@@ -1,10 +1,12 @@
 def identify(audio_file: str) -> str:
     """
     Identify a bird based on the sound of its call.
-    
-    :param audio_file: The path of an audio file.
-    
-    :return: The name of the bird species.
+
+    Args:
+        audio_file: The path of an audio file.
+
+    Returns:
+        The name of the bird species.
     """
     
     print("Identifying bird vocalisation...")
